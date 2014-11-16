bddbe334d6d18a4eb7dbe7192810aa4a5d798219
Lambda Phage <marwan.mohamed@rockets.utoledo.edu>
2014-11-15 20:35:03 -0500

    Program updated to fulfill assignment 2 requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are meant to cache a value for the
+## inverse of a matrix to decrease potential overhead.
 
-## Write a short comment describing this function
+## Create a matrix object that the inverse can be stored to.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse = NULL
+  set <- function(y){
+    x       <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(get = get, set = set,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of a matrix unless already cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if (!is.null(inverse)){
+    message("Retrieving cached value")
+    return(inverse)
+  }
+  matrix <- x$get()
+  inverse <- solve(matrix)
+  x$setinverse(inverse)
+  inverse
 }
